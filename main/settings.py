36a280d5f0d46203bc1874e924f93b59837936dc
anferoar96 <afromana@unal.edu.co>
2019-12-19 22:17:06 -0500

    Espero q ya

--- main/settings.py
+++ main/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'HOST': 'tutoria_db',
+        'HOST': 'tutorias-db',
         'PORT': 5432
     }
 }
